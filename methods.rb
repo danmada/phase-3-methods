1cd695f31acede5cf59e7cd90799749d56dc0fcc
danmada <danmada@gmail.com>
2021-07-19 14:55:49 -0500

    commit tree 849ca7d20ffe6162d89d6ee172fee60ca670ed02

--- methods.rb
+++ methods.rb
@@ -1,1 +1,34 @@
 # Your code here!
+def greet_programmer
+   puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!" 
+end
+
+greet ("Naureen")
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default
+
+def add(a, b) 
+     a + b 
+end
+
+add(2, 5)
+
+def halve(num)
+    if num.class == String
+    return nil
+end
+
+    num/2
+end
+
+halve(6)
+
+
